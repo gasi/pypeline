75de1680d1595b37e269bc59e5deb5c86e0bf205
Daniel Gasienica <daniel@gasienica.ch>
2011-03-26 12:28:49 -0700

    Add setup script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name="pypeline",
+      version="0.1.0",
+      description="pypeline encodes and tags your movies & TV shows for iTunes",
+      author="Daniel Gasienica",
+      author_email="daniel@gasienica.ch",
+      keywords="pypeline itunes m4v tags movies tvshows tv encoding handbrake",
+      url="http://github.com/gasi/pypeline",
+      py_modules=["pypeline/__init__"])
